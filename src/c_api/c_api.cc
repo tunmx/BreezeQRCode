84808676a66300d1d7b7a464a182ecf7621a0ecc
yanjingyu <yanjingyu@3cink.com>
2021-04-30 15:37:37 +0800

    add c-api

---
+++ src/c_api/c_api.cc
@@ -0,0 +1,5 @@
+//
+// Created by apple on 2021/4/30.
+//
+
+#include "c_api.h"
---
+++ src/c_api/c_api.h
@@ -0,0 +1,16 @@
+//
+// Created by apple on 2021/4/30.
+//
+
+#ifndef BREEZEQRCODE_C_API_H
+#define BREEZEQRCODE_C_API_H
+
+
+
+class c_api {
+
+};
+
+
+
+#endif //BREEZEQRCODE_C_API_H
